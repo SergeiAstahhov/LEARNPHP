7b9655478539dd3409b3ced7eef5385a7cb35cf1
SergeiAstahhov <sergei.astahhov@tptlive.ee>
2024-10-07 13:31:32 +0300

    sergei

---
+++ public/index.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Router;
+
+if (preg_match('/\.(?:png|jpg|jpeg|gif|css|js)$/', $_SERVER["REQUEST_URI"])) {
+    return false;   
+}
+
+
+
+
+spl_autoload_register(function ($class) {
+    $class = substr($class, 4); 
+    $class = str_replace('\\', '/', $class); 
+    require_once __DIR__ . "/../src/$class.php";
+});
+
+session_start();
+require 'helpers.php';
+
+require 'routes.php';
+
+
+$router = new Router($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
+$match = $router->match();
+if($match){
+    if(is_callable($match->action)){
+        call_user_func($match->action);
+    } else if (is_array($match->action) && count($match->action) === 2){
+        $class = $match->action[0]; // 'App\Controllers\PublicController'
+        $controller = new $class();
+        $method = $match->action[1]; // index
+        $controller->$method(); 
+    }
+} else {
+    echo '404';
+}
---
+++ src/Controllers/PostsController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\Post;
+
+
+class PostsController
+{
+
+    public function index()
+    {
+        $posts = Post::all();
+
+        view('posts/index', compact('posts'));
+    }
+
+    public function create(){
+        view('posts/create');
+    }
+
+    public function store(){
+        $post = new Post();
+        $post->title = $_POST['title'];
+        $post->body = $_POST['body'];
+        $post->save();
+        redirect('/admin/posts');
+    }
+    public function show($id) {
+        $post = Post::find($id);
+    
+        if ($post) {
+            view('posts/view', compact('post'));
+        } else {
+            echo "Post not found";
+        }
+    }   
+    public function edit(){
+        $post = Post::find($_GET['id']);
+        view('posts/edit', compact('post'));
+    }
+    public function update(){
+        $post = Post::find($_GET['id']);
+        $post->title = $_POST['title'];
+        $post->body = $_POST['body'];
+        $post->save();
+        redirect('/admin/posts');
+    }
+    public function destroy(){
+        $post = Post::find($_GET['id']);
+        $post->delete();
+        redirect('/admin/posts');
+    }
+}
